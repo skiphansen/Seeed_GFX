4508f4b5156303259a79b3c84975e4be699588db
Skip Hansen <skip@gfrn.org>
2026-05-27 09:54:17 -0700

    Flattened zillions of tiny icon headers into icons.h, deleted all unused icons.  Code size is unchanged but it's  **LOT** easier to deal with fewer files. Number of source controlled files before: 9422, after 37. Hugh win for tag file regeneration time!

--- 7_4_BWRY/owm/lib/DrawOwm/renderer.cpp
+++ 7_4_BWRY/owm/lib/DrawOwm/renderer.cpp
@@ -28,15 +28,7 @@
 #include FONT_HEADER
 
 // icon header files
-#include "icons/icons_16x16.h"
-#include "icons/icons_24x24.h"
-#include "icons/icons_32x32.h"
-#include "icons/icons_48x48.h"
-#include "icons/icons_64x64.h"
-#include "icons/icons_96x96.h"
-#include "icons/icons_128x128.h"
-#include "icons/icons_160x160.h"
-#include "icons/icons_196x196.h"
+#include "icons/icons.h"
 
 #ifndef ACCENT_COLOR
   #define ACCENT_COLOR TFT_BLACK
