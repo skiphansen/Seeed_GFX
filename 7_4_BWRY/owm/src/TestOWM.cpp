df60d8d5218ca583dc7e0614fafad1ca3b42cb0c
Skip Hansen <skip@gfrn.org>
2026-07-17 07:37:28 -0700

    Select TrueType test at run time rather than compile time.

--- 7_4_BWRY/owm/src/TestOWM.cpp
+++ 7_4_BWRY/owm/src/TestOWM.cpp
@@ -18,13 +18,10 @@ Here is the 6 colors you can display:
 #include <DrawOWM.h>
 #include "owm_response.h"
 
-#define TEST_TTF
-#ifdef TEST_TTF
-   #include <FS.h>
-   using namespace fs;
-   #include <truetype.h>
-   #include "LittleFS.h"
-#endif
+#include <FS.h>
+using namespace fs;
+#include <truetype.h>
+#include "LittleFS.h"
 
 #define ENABLE_LOGGING  1
 #if ENABLE_LOGGING && __has_include("logging.h") 
@@ -120,18 +117,68 @@ const LookupTbl_t LookupTbl[] = {
 };
 
 void DrawBoundingBox(int16_t xOffset,int16_t yOffset,int16_t Width,int16_t Height);
+void OwmDrawTest(int c);
+void TestTrueType(void);
+void ClearScreen(void);
 
 EPaper epaper;
 
-#ifndef TEST_TTF
 void setup() 
+{
+   Serial.begin(115200);
+   LittleFS.begin();
+   epaper.begin();
+
+   setenv("TZ", "PST8PDT", 1);
+   tzset();
+
+   while (!Serial);
+   delay(250);
+
+   while (true) {
+      int c = 0;
+      LOG_RAW(" t: TrueType test\n");
+      LOG_RAW(" 1: 400 x 300 - English\n");
+      LOG_RAW(" 2: 640 x 384 - English\n");
+      LOG_RAW(" 3: 800 x 480 - English\n");
+      LOG_RAW(" 4: 400 x 300 - German\n");
+      LOG_RAW(" 5: 640 x 384 - German\n");
+      LOG_RAW(" 6: 800 x 480 - German\n\n");
+      LOG_RAW("Press a key to start a test\n");
+      while (!Serial.available());
+      while (Serial.available()) {
+         c = Serial.read();
+      }
+
+      switch(c) {
+         case '1':
+         case '2':
+         case '3':
+         case '4':
+         case '5':
+         case '6':
+            OwmDrawTest(c);
+            break;
+
+         case 't':
+            TestTrueType();
+            break;
+
+         default:
+            LOG_RAW("\"%c\" is an invalid option\n\n",c);
+            continue;
+      }
+      epaper.update(); // update the display
+   }
+}
+
+void OwmDrawTest(int c)
 {
    OwmConfig Config;
    JsonDocument doc;
    JsonObject languageObject;
    const char *FormatDesc = NULL;
 
-   Serial.begin(115200);
 // City name that will be shown in the top-right corner of the display.
    Config.City = "Rancho Palos Verdes";
    Config.TimeFormat = "%l:%M %P";
@@ -148,247 +195,200 @@ void setup()
 //   Config.DisplayFormat = FORMAT_640X384;
    Config.DisplayFormat = FORMAT_800X480;
 
-   setenv("TZ", "PST8PDT", 1);
-   tzset();
+   Config.bDisplayAlerts = false;
+   switch (c) {
+      case '1':
+         Config.DisplayFormat = FORMAT_400X300;
+         Config.bMetric = false;
+         break;
 
-   while (!Serial);
-   delay(250);
-   LOG("Owm test\n");
+      case '2':
+         Config.DisplayFormat = FORMAT_640X384;
+         Config.bMetric = false;
+         Config.bDisplayAlerts = true;
+         break;
 
-   while (true) {
-      int c = 0;
-      LOG(" 1: 400 x 300 - English\n");
-      LOG(" 2: 640 x 384 - English\n");
-      LOG(" 3: 800 x 480 - English\n");
-      LOG(" 4: 400 x 300 - German\n");
-      LOG(" 5: 640 x 384 - German\n");
-      LOG(" 6: 800 x 480 - German\n");
-      LOG("Press a key to continue\n");
-      while (!Serial.available());
-      while (Serial.available()) {
-         c = Serial.read();
-      }
+      case '3':
+         Config.DisplayFormat = FORMAT_800X480;
+         Config.bMetric = false;
+         Config.bDisplayAlerts = true;
+         break;
 
-      Config.bDisplayAlerts = false;
-      switch(c) {
-         case '1':
-            Config.DisplayFormat = FORMAT_400X300;
-            Config.bMetric = false;
-            break;
+      case '4':
+         Config.DisplayFormat = FORMAT_400X300;
+         Config.bMetric = true;
+         break;
 
-         case '2':
-            Config.DisplayFormat = FORMAT_640X384;
-            Config.bMetric = false;
-            Config.bDisplayAlerts = true;
-            break;
+      case '5':
+         Config.DisplayFormat = FORMAT_640X384;
+         Config.bMetric = true;
+         break;
 
-         case '3':
-            Config.DisplayFormat = FORMAT_800X480;
-            Config.bMetric = false;
-            Config.bDisplayAlerts = true;
-            break;
+      case '6':
+         Config.DisplayFormat = FORMAT_800X480;
+         Config.bMetric = true;
+         break;
 
-         case '4':
-            Config.DisplayFormat = FORMAT_400X300;
-            Config.bMetric = true;
-            break;
-
-         case '5':
-            Config.DisplayFormat = FORMAT_640X384;
-            Config.bMetric = true;
-            break;
-
-         case '6':
-            Config.DisplayFormat = FORMAT_800X480;
-            Config.bMetric = true;
-            break;
-
-         default:
-            LOG("Invalid option\n");
-            continue;
-      }
-      Config.WindSpeed = Config.bMetric ? UNITS_SPEED_KILOMETERSPERHOUR : 
-                     UNITS_SPEED_MILESPERHOUR;
-      Config.DistanceType = Config.bMetric ? UNITS_DIST_KILOMETERS : UNITS_DIST_MILES;
-      Config.PrecipType = Config.bMetric ? UNITS_DAILY_PRECIP_MILLIMETERS : 
-                                       UNITS_DAILY_PRECIP_INCHES;
-
-      Config.PrecipHrType = Config.bMetric ? UNITS_HOURLY_PRECIP_MILLIMETERS :
-                                         UNITS_HOURLY_PRECIP_INCHES;
-      Config.PressureType = Config.bMetric ? UNITS_PRES_MILLIBARS :
-                                         UNITS_PRES_INCHESOFMERCURY;
-
-      switch(Config.DisplayFormat) {
-         case FORMAT_800X480:
-            Config.DisplayWidth    = 800;
-            Config.DisplayHeight   = 480;
-            Config.PosSunrise      = 0;
-            Config.PosSunset       = 1;
-            Config.PosWind         = 2;
-            Config.PosHumidity     = 3;
-            Config.PosUvi          = 4;
-            Config.PosPressure     = 5;
-            Config.PosAirQuality   = 6;
-            Config.PosVisibility   = 7;
-            Config.PosIntemp       = 8;
-            Config.PosInhumidity   = 9;
-            Config.PosMoonrise     = -1;
-            Config.PosMoonset      = -1;
-            Config.PosMoonphase    = -1;
-            Config.PosDewpoint     = -1;
-            break;
-
-         case FORMAT_640X384:
-     // if a 640 x 384 display is used, then positions 6,7,8,9 are not available
-            Config.DisplayWidth    = 640;
-            Config.DisplayHeight   = 384;
-            Config.PosSunrise      = 0;
-            Config.PosSunset       = 1;
-            Config.PosWind         = 2;
-            Config.PosHumidity     = 3;
-            Config.PosVisibility   = 4;
-            Config.PosIntemp       = 5;
-            Config.PosUvi          = -1;
-            Config.PosPressure     = -1;
-            Config.PosAirQuality   = -1;
-            Config.PosInhumidity   = -1;
-            Config.PosMoonrise     = -1;
-            Config.PosMoonset      = -1;
-            Config.PosMoonphase    = -1;
-            Config.PosDewpoint     = -1;
-            break;
-
-         case FORMAT_400X300:
-      // if a 400 x 300 display is used, then 12 positions available ???
-            Config.DisplayWidth    = 400;
-            Config.DisplayHeight   = 300;
-            Config.PosSunrise      = 0;
-            Config.PosSunset       = 1;
-            Config.PosWind         = 2;
-            Config.PosHumidity     = 3;
-            Config.PosUvi          = 4;
-            Config.PosPressure     = 5;
-            Config.PosAirQuality   = 6;
-            Config.PosVisibility   = 7;
-            Config.PosIntemp       = 8;
-            Config.PosInhumidity   = 9;
-            Config.PosMoonrise     = -1;
-            Config.PosMoonset      = -1;
-            Config.PosMoonphase    = -1;
-            Config.PosDewpoint     = -1;
-            break;
-      }
-
-      epaper.begin();
-// Work around bug in Seeed TFT_eSPI library, fillScreen() doesn't handle
-// rotation correctly.
-      epaper.setRotation(0);
-      epaper.fillScreen(TFT_WHITE);
-      epaper.setRotation(1);
-
-      int16_t xOffset = 0;
-      int16_t yOffset = 0;
-      switch(Config.DisplayFormat) {
-         case FORMAT_800X480:
-            FormatDesc = "800 x 480 ";
-            break;
-
-         case FORMAT_640X384:
-            FormatDesc = "640 x 384";
-            break;
-
-         case FORMAT_400X300:
-            FormatDesc = "400 x 300";
-            break;
-      }
-      xOffset = (800 - Config.DisplayWidth) / 2;
-      yOffset = (480 - Config.DisplayHeight) / 2;
-       
-      LOG("Width %d Height %d xOffset %d yOffset %d\n",
-          Config.DisplayWidth,Config.DisplayHeight,xOffset,yOffset);
-      Config.xOffset = xOffset;
-      Config.yOffset = yOffset;
-
-      if (Config.DisplayWidth != 800) {
-         DrawBoundingBox(xOffset,yOffset,Config.DisplayWidth,Config.DisplayHeight);
-      }
-
-      LOG("Updating %s res display in %s mode.\n",
-          FormatDesc,Config.bMetric ? "metric / German" : "english");
-
-      class DrawOWM *owm = epaper. DrawOWM(epaper,Config);
-      if(Config.bMetric) {
-         JsonDocument filter;
-         int Language = 2;
-         const char *Name;
-         const LookupTbl_t *p = LookupTbl;
-
-         filter[String(Language)] = true;
-         memset(&Strings,0,sizeof(Strings));
-
-         do {
-            DeserializationError error;
-            error = deserializeJson(doc,LanguagesJson,
-                                    DeserializationOption::Filter(filter));
-            if(error) {
-                LOG("Failed to parse JSON:\n%s\n",error.c_str());
-                break;
-            }
-            languageObject = doc[String(Language)];
-            for(int i = 0; i < 7; i++) {
-               Strings.LC_ABDAY[i] = languageObject["daysShort"][i].as<const char *>();
-               Strings.LC_DAY[i] = languageObject["days"][i].as<const char *>();
-            }
-            for(int i = 0; i < 12; i++) {
-               Strings.LC_MON[i] = languageObject["months"][i].as<const char *>();
-            }
-            while((Name = p->Name) != NULL) {
-               if(languageObject[Name]) {
-                  *p->Value = languageObject[Name].as<const char *>();
-               }
-               p++;
-            }
-            owm->SetLocale(&Strings);
-         } while(false);
-      }
-      owm->DrawIt();
-      delete owm;
-      epaper.update(); // update the display
+      default:
+         break;
    }
+   Config.WindSpeed = Config.bMetric ? UNITS_SPEED_KILOMETERSPERHOUR : 
+                      UNITS_SPEED_MILESPERHOUR;
+   Config.DistanceType = Config.bMetric ? UNITS_DIST_KILOMETERS : UNITS_DIST_MILES;
+   Config.PrecipType = Config.bMetric ? UNITS_DAILY_PRECIP_MILLIMETERS : 
+                       UNITS_DAILY_PRECIP_INCHES;
+
+   Config.PrecipHrType = Config.bMetric ? UNITS_HOURLY_PRECIP_MILLIMETERS :
+                         UNITS_HOURLY_PRECIP_INCHES;
+   Config.PressureType = Config.bMetric ? UNITS_PRES_MILLIBARS :
+                         UNITS_PRES_INCHESOFMERCURY;
+
+   switch (Config.DisplayFormat) {
+      case FORMAT_800X480:
+         Config.DisplayWidth    = 800;
+         Config.DisplayHeight   = 480;
+         Config.PosSunrise      = 0;
+         Config.PosSunset       = 1;
+         Config.PosWind         = 2;
+         Config.PosHumidity     = 3;
+         Config.PosUvi          = 4;
+         Config.PosPressure     = 5;
+         Config.PosAirQuality   = 6;
+         Config.PosVisibility   = 7;
+         Config.PosIntemp       = 8;
+         Config.PosInhumidity   = 9;
+         Config.PosMoonrise     = -1;
+         Config.PosMoonset      = -1;
+         Config.PosMoonphase    = -1;
+         Config.PosDewpoint     = -1;
+         break;
+
+      case FORMAT_640X384:
+         // if a 640 x 384 display is used, then positions 6,7,8,9 are not available
+         Config.DisplayWidth    = 640;
+         Config.DisplayHeight   = 384;
+         Config.PosSunrise      = 0;
+         Config.PosSunset       = 1;
+         Config.PosWind         = 2;
+         Config.PosHumidity     = 3;
+         Config.PosVisibility   = 4;
+         Config.PosIntemp       = 5;
+         Config.PosUvi          = -1;
+         Config.PosPressure     = -1;
+         Config.PosAirQuality   = -1;
+         Config.PosInhumidity   = -1;
+         Config.PosMoonrise     = -1;
+         Config.PosMoonset      = -1;
+         Config.PosMoonphase    = -1;
+         Config.PosDewpoint     = -1;
+         break;
+
+      case FORMAT_400X300:
+         // if a 400 x 300 display is used, then 12 positions available ???
+         Config.DisplayWidth    = 400;
+         Config.DisplayHeight   = 300;
+         Config.PosSunrise      = 0;
+         Config.PosSunset       = 1;
+         Config.PosWind         = 2;
+         Config.PosHumidity     = 3;
+         Config.PosUvi          = 4;
+         Config.PosPressure     = 5;
+         Config.PosAirQuality   = 6;
+         Config.PosVisibility   = 7;
+         Config.PosIntemp       = 8;
+         Config.PosInhumidity   = 9;
+         Config.PosMoonrise     = -1;
+         Config.PosMoonset      = -1;
+         Config.PosMoonphase    = -1;
+         Config.PosDewpoint     = -1;
+         break;
+   }
+
+   int16_t xOffset = 0;
+   int16_t yOffset = 0;
+   switch (Config.DisplayFormat) {
+   case FORMAT_800X480:
+      FormatDesc = "800 x 480 ";
+      break;
+
+   case FORMAT_640X384:
+      FormatDesc = "640 x 384";
+      break;
+
+   case FORMAT_400X300:
+      FormatDesc = "400 x 300";
+      break;
+   }
+   xOffset = (800 - Config.DisplayWidth) / 2;
+   yOffset = (480 - Config.DisplayHeight) / 2;
+
+   LOG("Width %d Height %d xOffset %d yOffset %d\n",
+       Config.DisplayWidth,Config.DisplayHeight,xOffset,yOffset);
+   Config.xOffset = xOffset;
+   Config.yOffset = yOffset;
+
+   if (Config.DisplayWidth != 800) {
+      DrawBoundingBox(xOffset,yOffset,Config.DisplayWidth,Config.DisplayHeight);
+   }
+
+   LOG("Updating %s res display in %s mode.\n",
+       FormatDesc,Config.bMetric ? "metric / German" : "english");
+
+   ClearScreen();
+   class DrawOWM *owm = new DrawOWM(epaper,Config);
+   if (Config.bMetric) {
+      JsonDocument filter;
+      int Language = 2;
+      const char *Name;
+      const LookupTbl_t *p = LookupTbl;
+
+      filter[String(Language)] = true;
+      memset(&Strings,0,sizeof(Strings));
+
+      do {
+         DeserializationError error;
+         error = deserializeJson(doc,LanguagesJson,
+                                 DeserializationOption::Filter(filter));
+         if (error) {
+            LOG("Failed to parse JSON:\n%s\n",error.c_str());
+            break;
+         }
+         languageObject = doc[String(Language)];
+         for (int i = 0; i < 7; i++) {
+            Strings.LC_ABDAY[i] = languageObject["daysShort"][i].as<const char *>();
+            Strings.LC_DAY[i] = languageObject["days"][i].as<const char *>();
+         }
+         for (int i = 0; i < 12; i++) {
+            Strings.LC_MON[i] = languageObject["months"][i].as<const char *>();
+         }
+         while ((Name = p->Name) != NULL) {
+            if (languageObject[Name]) {
+               *p->Value = languageObject[Name].as<const char *>();
+            }
+            p++;
+         }
+         owm->SetLocale(&Strings);
+      } while (false);
+   }
+   owm->DrawIt();
+   delete owm;
 }
-#else
-void setup() 
+
+
+void TestTrueType() 
 {
-   Serial.begin(115200);
-   while (!Serial);
-   delay(250);
-
    uint8_t Err;
-   LittleFSFS LittleFS;
-
-   epaper.begin();
-// Work around bug in Seeed TFT_eSPI library, fillScreen() doesn't handle
-// rotation correctly.
-   epaper.setRotation(0);
-   epaper.fillScreen(TFT_WHITE);
-   epaper.setRotation(1);
-
-   int16_t height = epaper.width();
-   int16_t width = epaper.height();
-
-   while (!Serial.available());
-   while (Serial.available()) {
-      Serial.read();
-   }
 
    LOG("TrueType font test\n");
 
+   ClearScreen();
+   int16_t height = epaper.width();
+   int16_t width = epaper.height();
    truetypeClass truetype = truetypeClass();
    void *framebuffer = epaper.getPointer();
    LOG("framebuffer %p\n",framebuffer);
    truetype.setFramebuffer(width,height,epaper.getColorDepth(), static_cast<uint8_t *>(framebuffer));
-   LittleFS.begin();
-   File fontFile = LittleFS.open("/FreeSans-utf8.ttf","r");
+   File fontFile = LittleFS.open("/fonts/FreeSans-utf8.ttf","r");
 
    if((Err = truetype.setTtfFile(fontFile)) == 0) {
       LOG("setTtfFile returned %d\n",Err);
@@ -401,7 +401,7 @@ void setup()
    truetype.textDraw(0,0,"TrueType Test");
    truetype.end();
 
-   fontFile = LittleFS.open("/weathericons.ttf","r");
+   fontFile = LittleFS.open("/fonts/weathericons.ttf","r");
 
    if((Err = truetype.setTtfFile(fontFile)) == 0) {
       LOG("setTtfFile returned %d\n",Err);
@@ -424,10 +424,17 @@ void setup()
       y += 32;
    }
    truetype.end();
-   LittleFS.end();
    epaper.update(); // update the display
 }
-#endif
+
+void ClearScreen()
+{
+// Work around bug in Seeed TFT_eSPI library, fillScreen() doesn't handle
+// rotation correctly.
+   epaper.setRotation(0);
+   epaper.fillScreen(TFT_WHITE);
+   epaper.setRotation(1);
+}
 
 void loop()
 {
